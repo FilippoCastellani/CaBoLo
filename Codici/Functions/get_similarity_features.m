8dbc48593297d1658fdab45a98fd249ceb86dbe7
Antonella Lombardi <antolombardi18@gmail.com>
2023-05-06 12:16:14 +0200

    Create get_similarity_features.m

---
+++ Codici/Functions/get_similarity_features.m
@@ -0,0 +1,15 @@
+function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_similarity_features(ecg, fs, Rpeak_index)
+
+% This function calculates the Similarity Features: Similarity index between beats
+
+% OUTPUT
+
+% [ ] - Similarity index of QRS (QRS_similarity)
+% [ ] - Similarity index of R amplitude (R_similarity)
+% [ ] - Ratio of high similarity beats (HighBeats_similarity)
+% [ ] - Signal Qualify index (SQindex)
+
+
+
+
+end
