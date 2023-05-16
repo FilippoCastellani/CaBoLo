4f1ff621868107497abfe9d309f4b8bb78634b24
Antonella Lombardi <antolombardi18@gmail.com>
2023-05-16 18:55:26 +0200

    Update get_similarity_features.m
    Here the function to get all the similarity feature, a call to the get_similarity for QRS complex and R amplitude similarity, and the computation of High Similarity beat ratio, and Signal Quality Index

--- Codici/Functions/get_similarity_features.m
+++ Codici/Functions/get_similarity_features.m
@@ -1,4 +1,4 @@
-function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_similarity_features(ecg, fs, Rpeak_index)
+function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_similarity_features(ecg, fs, t, Rpeak_index, visuals)
 
 % This function calculates the Similarity Features: Similarity index between beats
 
@@ -6,8 +6,8 @@ function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_sim
 
 % [X] - Similarity index of QRS (QRS_similarity)
 % [X] - Similarity index of R amplitude (R_similarity)
-% [ ] - Ratio of high similarity beats (HighBeats_similarity)
-% [ ] - Signal Qualify index (SQindex)
+% [X] - Ratio of high similarity beats (HighBeats_similarity)
+% [X] - Signal Qualify index (SQindex)
 
 % INPUT
 
@@ -18,14 +18,36 @@ function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_sim
 %%
 
 % Get fiducial points of interest
-[~, ~, ~, ~, ~, ~, ~, ~, R_peak, QRS_onset, QRS_offset]= get_fiducial_points(ecg, Rpeak_index, fs);
+[~, P_onset, ~, ~, ~, T_offset, ~, ~, R_peak, QRS_onset, QRS_offset]= get_fiducial_points(ecg, Rpeak_index, fs);
 
 % Compute similarity between QRS complex
-QRS_similarity = get_correlation(ecg, QRS_onset, QRS_offset, 0);
+QRS_similarity_vector = get_similarity(ecg, QRS_onset, QRS_offset, 0);
+QRS_similarity = mean(QRS_similarity_vector);
 
 % Compute similarity between R peaks
-R_similarity = get_correlation(ecg, R_peak(1:end-1), R_peak(2:end), 1);
+R_similarity_vector = get_similarity(ecg, R_peak, R_peak, 1);
+R_similarity = mean(R_similarity_vector);
 
+% Compute High Beats Similarity
+thresholdQRS = 0.40;
+thresholdR = 0.55;
+HighBeats_similarity = 0;
+for i = 1:length(QRS_similarity_vector)
+    if QRS_similarity_vector(i) > thresholdQRS && R_similarity_vector(i) > thresholdR
+        HighBeats_similarity = HighBeats_similarity + 1;
+    end
+end
 
+HighBeats_similarity = HighBeats_similarity/length(QRS_similarity_vector);
+
+% Compute Signal Qualify Index
+% Initialize the vector
+SQindex_vector = zeros(length(P_onset)-1, 1);
+for i = 2:(length(P_onset)-1)
+    SQindex_vector(i) = abs(ecg(P_onset(i))-ecg(T_offset(i-1)));
+end
+
+% Signal Qualify Index as the mean of the SQindex of each beat
+SQindex = mean(SQindex_vector);
 
 end
