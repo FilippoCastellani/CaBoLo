e57647bd245488f735eb6f9a12c84ae79c526e9a
Antonella Lombardi <antolombardi18@gmail.com>
2023-05-19 16:52:36 +0200

    Update get_similarity_features.m
    Ratio of High beats similarity calculated on the product of QRS and R amplitude similarity, threshold is calculated as the 60th percentile of the product similarity vector.

--- Codici/Functions/get_similarity_features.m
+++ Codici/Functions/get_similarity_features.m
@@ -1,4 +1,4 @@
-function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_similarity_features(ecg, fs, t, Rpeak_index, visuals)
+function [QRS_similarity, R_similarity, Ratio_HBS, SQindex] = get_similarity_features(ecg, fs, t, Rpeak_index, visuals)
 
 % This function calculates the Similarity Features: Similarity index between beats
 
@@ -6,7 +6,7 @@ function [QRS_similarity, R_similarity, HighBeats_similarity, SQindex] = get_sim
 
 % [X] - Similarity index of QRS (QRS_similarity)
 % [X] - Similarity index of R amplitude (R_similarity)
-% [X] - Ratio of high similarity beats (HighBeats_similarity)
+% [X] - Ratio of high similarity beats (Ratio_HBS)
 % [X] - Signal Qualify index (SQindex)
 
 % INPUT
@@ -29,25 +29,25 @@ R_similarity_vector = get_similarity(ecg, R_peak, R_peak, 1);
 R_similarity = mean(R_similarity_vector);
 
 % Compute High Beats Similarity
-thresholdQRS = 0.40;
-thresholdR = 0.55;
+similarity_vector = QRS_similarity_vector .* R_similarity_vector;
+threshold_similarity = prctile(similarity_vector, 60);
 HighBeats_similarity = 0;
-for i = 1:length(QRS_similarity_vector)
-    if QRS_similarity_vector(i) > thresholdQRS && R_similarity_vector(i) > thresholdR
+for i = 1:length(similarity_vector)
+    if similarity_vector(i) > threshold_similarity
         HighBeats_similarity = HighBeats_similarity + 1;
     end
 end
 
-HighBeats_similarity = HighBeats_similarity/length(QRS_similarity_vector);
+Ratio_HBS = HighBeats_similarity/length(similarity_vector);
 
 % Compute Signal Qualify Index
 % Initialize the vector
 SQindex_vector = zeros(length(P_onset)-1, 1);
 for i = 2:(length(P_onset)-1)
-    SQindex_vector(i) = abs(ecg(P_onset(i))-ecg(T_offset(i-1)));
+    SQindex_vector(i) = ecg(P_onset(i))-ecg(T_offset(i-1));
 end
 
 % Signal Qualify Index as the mean of the SQindex of each beat
-SQindex = mean(SQindex_vector);
+SQindex = std(SQindex_vector);
 
 end
