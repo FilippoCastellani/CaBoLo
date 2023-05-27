71c7b7fa710ee0bb9371d3669d8cb5e7efb6ebc8
FilippoCastellani <filippo.castellani@mail.polimi.it>
2023-05-27 13:29:10 +0200

    Main classification update

--- Codici/main_classification.m
+++ Codici/main_classification.m
@@ -4,6 +4,20 @@
 clc; close all; clear;
 
 % Setting the environment
-[PROJECT_DIRECTORY, DatasetFolderPath] = set_environment();
+[PROJECT_DIRECTORY, DatasetFolderPath, DatasetFolderPrefix] = set_environment();
 
 %% Load data 
+% load the .csv file containing the features matrix along with the labels
+
+DATASET = readtable('features_PROVA_TEST.csv');
+
+X = DATASET(:, 1:end-1);
+Y = DATASET(:, end);
+
+%% 
+% For each model define the range of value in which the hyperparameters will be searched 
+% and the number of iterations for the search (considering that we will be using a random search)
+
+% Define the number of folds = 5 for the cross-validation 
+
+% train a model using the whole training data in order to compare it by the end with the performance on the test data.
