214f2ca97828bfcd1b37a7fc66c6ab97b54cbea6
FilippoCastellani <filippo.castellani@mail.polimi.it>
2023-06-11 10:39:12 +0200

    update to main classification (THIS VERSION IS NOT WORKING)

--- Codici/main_classification.m
+++ Codici/main_classification.m
@@ -8,16 +8,55 @@ clc; close all; clear;
 
 %% Load data 
 % load the .csv file containing the features matrix along with the labels
+% we need to load both the training and the test data
 
-DATASET = readtable('features_PROVA_TEST.csv');
+DATASET_TRAIN = readtable('Data/train_features_complete.csv');
 
-X = DATASET(:, 1:end-1);
-Y = DATASET(:, end);
+X_TRAIN = DATASET_TRAIN(:, 1:end-1);
+Y_TRAIN = DATASET_TRAIN(:, end);
+
+DATASET_TEST = readtable('Data/test_features_complete.csv');
+X_TEST = DATASET_TEST(:, 1:end-1);
+Y_TEST = DATASET_TEST(:, end);
+
+%% We are going to train a decision tree model 
+
+% Create the hyperparameter optimization options and performing grid search
+hyperparameter_options= struct('Optimizer', 'gridsearch', 'ShowPlots', true);
+hyperparameters = struct('MaxNumSplits', [5, 10, 15, 20 ,25 ,30, 35], 'MinLeafSize', [1, 5, 10, 15 ,20]);
+tree = fitctree(X_TRAIN, Y_TRAIN, 'OptimizeHyperparameters', hyperparameters, 'HyperparameterOptimizationOptions', hyperparameter_options);
+
+
+% Training the model
+% tree_model = fitctree(X_TRAIN, Y_TRAIN, 'OptimizeHyperparameters','all',...
+%    'HyperparameterOptimizationOptions',struct('Kfold',5,'Optimizer','randomsearch'));
 
 %% 
-% For each model define the range of value in which the hyperparameters will be searched 
-% and the number of iterations for the search (considering that we will be using a random search)
+% Predicting the labels for the test data
+Y_TEST_PREDICTED = predict(tree_model, X_TEST);
+error_test = loss(tree_model, X_TEST, Y_TEST_PREDICTED);
+conf_mat = confusionmat(char(Y_TEST.label), char(Y_TEST_PREDICTED));
+accuracy = sum(diag(conf_mat))/sum(conf_mat, 'all');
 
-% Define the number of folds = 5 for the cross-validation 
+%% 
+% Now we are going to train a model using the AdaBoost algorithm
+% The AdaBoost algorithm is an algorithm that combines multiple weak
+% classifiers to create a strong classifier. A weak classifier is simply a
+% classifier that performs poorly, but performs better than random guessing
+% (i.e. it has an accuracy of > 50%). The AdaBoost algorithm combines
+% multiple weak classifiers to create a strong classifier.
 
-% train a model using the whole training data in order to compare it by the end with the performance on the test data.
+% Training the model
+%adaboost_model = fitcensemble(X_TRAIN, Y_TRAIN, 'OptimizeHyperparameters','all');
+
+adaboost_model = fitcensemble(X_TRAIN, Y_TRAIN, 'Method','AdaBoostM2',...
+    'OptimizeHyperparameters','all',...
+    'HyperparameterOptimizationOptions',struct('Kfold',5,'Optimizer','randomsearch'));
+
+%%
+% Predicting the labels for the test data
+Y_TEST_PREDICTED = predict(adaboost_model, X_TEST);
+error_test = loss(adaboost_model, X_TEST, Y_TEST_PREDICTED);
+conf_mat = confusionmat(char(Y_TEST.label), char(Y_TEST_PREDICTED));
+accuracy = sum(diag(conf_mat))/sum(conf_mat, 'all');
+
