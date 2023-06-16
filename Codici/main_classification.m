fb85ba100a971d6e8e96950d074c64e6aa750982
ChiaraBoscarino <chiaraboscarino20@gmail.com>
2023-06-16 10:20:01 +0200

    settato l'env per mainardino

--- Codici/main_classification.m
+++ Codici/main_classification.m
@@ -3,8 +3,94 @@
 % Clearing previous data and closing all the windows if any
 clc; close all; clear;
 
-% Setting the environment
-[PROJECT_DIRECTORY, DatasetFolderPath, DatasetFolderPrefix] = set_environment();
+%% Setting the environment
+
+% Set environment for host
+host_path = 'C:/Users/c.boscarino/OneDrive - Reply/Desktop/ChiaraBoscarino/UNI/BSPLab/CaBoLo/Codici/'; 
+
+% Set working path
+    if (~isempty(host_path)) 
+        PROJECT_DIRECTORY = host_path;
+        cd(PROJECT_DIRECTORY);
+        
+        % add WFDB toolbox to search path
+        addpath(genpath([PROJECT_DIRECTORY 'WFDB_Toolbox']));
+
+        % add functions folder to search path
+        addpath(genpath([PROJECT_DIRECTORY 'Functions']));
+        
+        % identify Dataset folder within project directory
+        DatasetFolderPrefix = 'Dataset/training2017/';
+        DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+        disp('Environment set to hostpath');
+
+    elseif(getenv('COMPUTERNAME')=="FILIPPO") % se sei sul computer di filippo
+        PROJECT_DIRECTORY = 'E:/ProgettiGithub/CaBoLo/Codici/';
+        cd(PROJECT_DIRECTORY);
+        
+        % add WFDB toolbox to search path
+        addpath(genpath("E:/ProgettiGithub/CaBoLo/Codici/WFDB_Toolbox"));
+
+        % add functions folder to search path
+        addpath(genpath("E:/ProgettiGithub/CaBoLo/Codici/Functions"));
+        
+        % identify Dataset folder within project directory
+        DatasetFolderPrefix = 'Dataset/training2017/';
+        DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+    
+    elseif(getenv('COMPUTERNAME')=="DESKTOP-T9UVACS")
+        PROJECT_DIRECTORY = 'C:/Users/Filippo/Documents/GitHub/CaBoLo/Codici';
+        cd(PROJECT_DIRECTORY);
+        
+        % add WFDB toolbox to search path
+        addpath(genpath("C:/Users/Filippo/Documents/GitHub/CaBoLo/Codici/WFDB_Toolbox"));
+
+        % add functions folder to search path
+        addpath(genpath("C:/Users/Filippo/Documents/GitHub/CaBoLo/Codici/Functions"));
+         
+        DatasetFolderPrefix = 'Dataset/training2017/';
+        DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+        
+        elseif(getenv('COMPUTERNAME')=="F-C")
+        PROJECT_DIRECTORY = 'C:/Users/caste/CaBoLo_Git/CaBoLo/Codici/';
+        cd(PROJECT_DIRECTORY);
+        
+        % add WFDB toolbox to search path
+        addpath(genpath("C:/Users/caste/CaBoLo_Git/CaBoLo/Codici/WFDB_Toolbox"));
+
+        % add functions folder to search path
+        addpath(genpath("C:/Users/caste/CaBoLo_Git/CaBoLo/Codici/Functions"));
+         
+        DatasetFolderPrefix = 'Dataset/training2017/';
+        DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+        
+      
+    elseif(getenv('COMPUTERNAME')=="") % Computer Nelly
+         PROJECT_DIRECTORY = '/Users/antonellalombardi/Documents/GitHub/CaBoLo/Codici/';
+         cd(PROJECT_DIRECTORY);
+
+         addpath(genpath("/Users/antonellalombardi/Documents/GitHub/CaBoLo/Codici/WFDB_Toolbox"));
+         addpath(genpath("/Users/antonellalombardi/Documents/GitHub/CaBoLo/Codici/Functions"));
+         
+         DatasetFolderPrefix = 'Dataset/training2017/';
+         DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+    
+    elseif(getenv('COMPUTERNAME')=="HEAL26821") % Computer Bosca
+        PROJECT_DIRECTORY = 'C:/Users/c.boscarino/OneDrive - Reply/Desktop/ChiaraBoscarino/UNI/BSPLab/CaBoLo/Codici/';
+        cd(PROJECT_DIRECTORY);
+        
+        % add WFDB toolbox to search path
+        addpath(genpath("C:/Users/c.boscarino/OneDrive - Reply/Desktop/ChiaraBoscarino/UNI/BSPLab/CaBoLo/Codici/WFDB_Toolbox"));
+
+        % add functions folder to search path
+        addpath(genpath("C:/Users/c.boscarino/OneDrive - Reply/Desktop/ChiaraBoscarino/UNI/BSPLab/CaBoLo/Codici/Functions"));
+        
+        % identify Dataset folder within project directory
+        DatasetFolderPrefix = 'Dataset/training2017/';
+        DatasetFolderPath = [PROJECT_DIRECTORY DatasetFolderPrefix];
+
+    end
+
 
 %% Load data 
 % load the .csv file containing the features matrix along with the labels
